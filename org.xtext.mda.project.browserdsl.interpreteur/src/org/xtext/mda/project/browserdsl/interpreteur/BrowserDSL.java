a32b92a65d506d9e91e239cbc905247816005175
mcartaud <mathieu.cartaud@gmail.com>
2015-02-13 08:46:13 +0100

    create an action to interpret the model

---
+++ org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/BrowserDSL.java
@@ -0,0 +1,14 @@
+package org.xtext.mda.project.browserdsl.interpreteur;
+
+import org.eclipse.emf.ecore.EObject;
+
+public class BrowserDSL {
+
+	public static void start(EObject eobject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/actions/InterpreterAction.java
@@ -0,0 +1,86 @@
+package org.xtext.mda.project.browserdsl.interpreteur.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.xtext.mda.project.browserdsl.interpreteur.BrowserDSL;
+
+
+/**
+ * Our sample action implements workbench action delegate. The action proxy will
+ * be created by the workbench and shown in the UI. When the user tries to use
+ * the action, this delegate will be created and execution will be delegated to
+ * it.
+ * 
+ * @see IWorkbenchWindowActionDelegate
+ */
+public class InterpreterAction implements IWorkbenchWindowActionDelegate {
+	private IWorkbenchWindow window;
+
+	/**
+	 * The constructor.
+	 */
+	public InterpreterAction() {
+	}
+
+	/**
+	 * The action has been activated. The argument of the method represents the
+	 * 'real' action sitting in the workbench UI.
+	 * 
+	 * @see IWorkbenchWindowActionDelegate#run
+	 */
+	public void run(IAction action) {
+		
+		IEditorPart activeEditor = window.getActivePage().getActiveEditor(); 
+
+		IFile file = (IFile) activeEditor.getEditorInput().getAdapter(IFile.class); 
+
+		System.out.println("Loading file: " + file.getFullPath().toString());
+
+		ResourceSet rs = new ResourceSetImpl();
+
+		Resource resource = rs.getResource(URI.createURI(file.getFullPath().toString()), true);
+
+		EObject eobject = resource.getContents().get(0);
+		
+		BrowserDSL.start(eobject);
+	}
+
+	/**
+	 * Selection in the workbench has been changed. We can change the state of
+	 * the 'real' action here if we want, but this can only happen after the
+	 * delegate has been created.
+	 * 
+	 * @see IWorkbenchWindowActionDelegate#selectionChanged
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		
+	}
+
+	/**
+	 * We can use this method to dispose of any system resources we previously
+	 * allocated.
+	 * 
+	 * @see IWorkbenchWindowActionDelegate#dispose
+	 */
+	public void dispose() {
+	}
+
+	/**
+	 * We will cache window object in order to be able to provide parent shell
+	 * for the message dialog.
+	 * 
+	 * @see IWorkbenchWindowActionDelegate#init
+	 */
+	public void init(IWorkbenchWindow window) {
+		this.window = window;
+	}
+}
