1a1633c8205d55ccdb21c3d97e14005b33cc9e36
bonnefont <bonnefont.julien@gmail.com>
2015-02-21 17:36:37 +0100

    update from element to list

--- org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/model/ComputePicture.java
+++ org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/model/ComputePicture.java
@@ -1,41 +1,63 @@
 package org.xtext.mda.project.browserdsl.interpreteur.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.xtext.mda.project.browserDSL.Click;
+import org.xtext.mda.project.browserDSL.PictureAction;
 import org.xtext.mda.project.browserDSL.Pictures;
 import org.xtext.mda.project.browserdsl.interpreteur.BrowserDSL;
 import org.xtext.mda.project.browserdsl.interpreteur.utils.Utils;
 
 public class ComputePicture {
-	
-	private Pictures picture;
-	
+
+	private Pictures pictures;
+
 	public ComputePicture(Pictures instruction) {
-		this.picture = instruction;
+		this.pictures = instruction;
 	}
-	
-	public void executePictureAction() throws Exception{
-		String id = Utils.getElementId(picture.getId());
+
+	public void executePictureAction() throws Exception {
+		String id = Utils.getElementId(pictures.getId());
 		List<WebElement> elements = findGoodElements(id);
-		for (WebElement element : elements) {
-			element.click();
+		executeAction(elements);
+	}
+
+	private List<WebElement> findGoodElements(String id) throws Exception {
+
+		List<WebElement> elements = new ArrayList<WebElement>();
+		List<WebElement> elementsFound = BrowserDSL.getBrowser().findElements(By.cssSelector("img"));
+		for (WebElement element : elementsFound) {
+			if (element.getAttribute("src").toLowerCase().trim().contains(id.toLowerCase())) {
+				elements.add(element);
+			}
+		}
+
+		if (elements.size() == 0) {
+			elementsFound = BrowserDSL.getBrowser().findElements(By.cssSelector("area"));
+			for (WebElement element : elementsFound) {
+				if (element.getAttribute("title").toLowerCase().trim().contains(id.toLowerCase())) {
+					elements.add(element);
+				}
+			}
+		}
+
+		if (elements.size() != 0) {
+			return elements;
+		} else {
+			// TODO exception element not found
+			throw new Exception();
 		}
 	}
-	
-	private List<WebElement> findGoodElements(String id) throws Exception {
-		
-		List<WebElement> elements = BrowserDSL.getBrowser().findElements(By.xpath("//img[contains(@src, \""+id+"\")]"));
-		if(elements.size()==0){
-			elements = BrowserDSL.getBrowser().findElements(By.xpath("//area[contains(@title, '"+id+"','i')]"));
-		}
-		
-		if(elements.size()!=0){
-			return elements;
-		}else{
-			//TODO exception element not found
-			throw new Exception();
+
+	private void executeAction(List<WebElement> elements) throws Exception {
+		PictureAction action = pictures.getAction();
+		if (action instanceof Click) {
+			for (WebElement element : elements) {
+				element.click();
+			}
 		}
 	}
 }
--- org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/model/ComputeTextField.java
+++ org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/model/ComputeTextField.java
@@ -1,5 +1,6 @@
 package org.xtext.mda.project.browserdsl.interpreteur.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -22,21 +23,22 @@ public class ComputeTextField {
 	
 	public void executeTextFieldAction() throws Exception {
 		String id = Utils.getElementId(textField.getId());
-		WebElement element = findGoodElement(id);
-		executeAction(element);
+		List<WebElement> elements = findGoodElements(id);
+		executeAction(elements);
 	}
 
-	private WebElement findGoodElement(String id) {
-		List<WebElement> elements = BrowserDSL.getBrowser().findElements(By.name(id));
-		for (WebElement element : elements) {
+	private List<WebElement> findGoodElements(String id) {
+		List<WebElement> elements = new ArrayList<WebElement>();
+		List<WebElement> elementsFound = BrowserDSL.getBrowser().findElements(By.cssSelector("input[type='text']"));
+		for (WebElement element : elementsFound) {
 			if (element.getAttribute("name").toLowerCase().trim().contains(id.toLowerCase())) {
-				return element;
+				elements.add(element);
 			}
 		}	
-		return null;
+		return elements;
 	}
 	
-	private void executeAction(WebElement element) throws Exception {
+	private void executeAction(List<WebElement> elements) throws Exception {
 		TextAction action = textField.getAction();
 		if (action instanceof Fill) {
 			String value = Utils.getElementId(((Fill) action).getValue());
@@ -44,7 +46,7 @@ public class ComputeTextField {
 		} else if (action instanceof Clear) {
 			element.clear();
 		} else if (action instanceof CheckValue) {
-			// TODO
+			//TODO
 		}
 	}
 	
