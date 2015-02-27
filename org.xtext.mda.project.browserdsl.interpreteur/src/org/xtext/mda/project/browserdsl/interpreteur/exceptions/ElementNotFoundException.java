a0ed58b98f51c23cd2efaed299a08ef1eee85591
bonnefont <bonnefont.julien@gmail.com>
2015-02-27 17:25:02 +0100

    add error messages in user console

--- org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/exceptions/ElementNotFoundException.java
+++ org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/exceptions/ElementNotFoundException.java
@@ -1,5 +1,7 @@
 package org.xtext.mda.project.browserdsl.interpreteur.exceptions;
 
+import org.xtext.mda.project.browserdsl.interpreteur.utils.ConsoleUtils;
+
 public class ElementNotFoundException extends Exception {
 	
 	private static final long serialVersionUID = 9125463078008946652L;
@@ -8,6 +10,7 @@ public class ElementNotFoundException extends Exception {
 	
 	public ElementNotFoundException(String message) {
         super("L'element \"" + message + "\" ne peut pas etre trouve");
+        ConsoleUtils.writeToConsole("L'element \"" + message + "\" ne peut pas etre trouve");
     }
 
     public ElementNotFoundException(Throwable cause) {
---
+++ org.xtext.mda.project.browserdsl.interpreteur/src/org/xtext/mda/project/browserdsl/interpreteur/utils/ConsoleUtils.java
@@ -0,0 +1,53 @@
+package org.xtext.mda.project.browserdsl.interpreteur.utils;
+
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+
+/**
+ * Class used to print message to the user in the plugin
+ * 
+ */
+public class ConsoleUtils {
+
+	private static MessageConsole findConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++)
+			if (name.equals(existing[i].getName()))
+				return (MessageConsole) existing[i];
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
+
+	public static void writeToConsole(String msg) {
+		MessageConsole myConsole = findConsole("Interpreter");
+		MessageConsoleStream out = myConsole.newMessageStream();
+		out.println(msg);
+		out.println();
+
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+		String id = IConsoleConstants.ID_CONSOLE_VIEW;
+		try {
+			IConsoleView view = (IConsoleView) page.showView(id);
+			view.display(myConsole);
+		} catch (PartInitException e) {
+			throw new RuntimeException(e.getLocalizedMessage());
+		}
+	}
+
+}
